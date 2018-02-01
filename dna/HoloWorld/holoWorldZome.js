6246131b65071736654bec3521e78cd3a792a5a4
Connor Turland <connorturland@gmail.com>
2018-02-01 23:35:16 +0000

    Update holoWorldZome.js

--- dna/HoloWorld/holoWorldZome.js
+++ dna/HoloWorld/holoWorldZome.js
@@ -1,15 +1,25 @@
 'use strict';
 
-// -----------------------------------------------------------------
-//  This stub Zome code file was auto-generated
-// -----------------------------------------------------------------
+// EXPOSED METHODS
+
+// creates a holoWorldEntry entry
+function holoWorldEntryCreate (entry) {
+  return commit('holoWorldEntry', entry)
+}
+
+// retrieves a holoWorldEntry entry
+function holoWorldEntryRead (hash) {
+  // at the moment, to return the entry as JSON
+  // we have to use JSON.parse because the entry is a string
+  // soon this will be fixed and the JSON.parse can be removed
+  return JSON.parse(get(hash))
+}
 
 /**
  * Called only when your source chain is generated
  * @return {boolean} success
  */
 function genesis() {
-  // any genesis code here
   return true;
 }
 
@@ -17,117 +27,54 @@ function genesis() {
 //  validation functions for every DHT entry change
 // -----------------------------------------------------------------
 
-/**
- * Called to validate any changes to the DHT
- * @param {string} entryName - the name of entry being modified
- * @param {*} entry - the entry data to be set
- * @param {?} header - ?
- * @param {?} pkg - ?
- * @param {?} sources - ?
- * @return {boolean} is valid?
- */
 function validateCommit (entryName, entry, header, pkg, sources) {
   switch (entryName) {
     case 'holoWorldEntry':
-      // validation code here
+      // in order for the 'commit' action to work, validateCommit (given a holoWorldEntry) must return true
+      // there is no special validation that we have to perform for our simple app
       return true
     default:
-      // invalid entry name!!
+      // invalid entry name
       return false
   }
 }
 
-/**
- * Called to validate any changes to the DHT
- * @param {string} entryName - the name of entry being modified
- * @param {*}entry - the entry data to be set
- * @param {?} header - ?
- * @param {?} pkg - ?
- * @param {?} sources - ?
- * @return {boolean} is valid?
- */
 function validatePut (entryName, entry, header, pkg, sources) {
   switch (entryName) {
     case 'holoWorldEntry':
-      // validation code here
       return true
     default:
-      // invalid entry name!!
+      // invalid entry name
       return false
   }
 }
 
-/**
- * Called to validate any changes to the DHT
- * @param {string} entryName - the name of entry being modified
- * @param {*} entry- the entry data to be set
- * @param {?} header - ?
- * @param {*} replaces - the old entry data
- * @param {?} pkg - ?
- * @param {?} sources - ?
- * @return {boolean} is valid?
- */
 function validateMod (entryName, entry, header, replaces, pkg, sources) {
   switch (entryName) {
     case "sampleEntry":
-      // validation code here
       return false;
     default:
-      // invalid entry name!!
+      // invalid entry name
       return false;
   }
 }
 
-/**
- * Called to validate any changes to the DHT
- * @param {string} entryName - the name of entry being modified
- * @param {string} hash - the hash of the entry to remove
- * @param {?} pkg - ?
- * @param {?} sources - ?
- * @return {boolean} is valid?
- */
 function validateDel (entryName,hash, pkg, sources) {
   switch (entryName) {
     case "sampleEntry":
-      // validation code here
-return false;
+      return false;
     default:
-      // invalid entry name!!
+      // invalid entry name
       return false;
   }
 }
 
-/**
- * Called to get the data needed to validate
- * @param {string} entryName - the name of entry to validate
- * @return {*} the data required for validation
- */
 function validatePutPkg (entryName) {
   return null;
 }
-
-/**
- * Called to get the data needed to validate
- * @param {string} entryName - the name of entry to validate
- * @return {*} the data required for validation
- */
 function validateModPkg (entryName) {
   return null;
 }
-
-/**
- * Called to get the data needed to validate
- * @param {string} entryName - the name of entry to validate
- * @return {*} the data required for validation
- */
 function validateDelPkg (entryName) {
   return null;
-}
-
-function holoWorldEntryCreate (entry) {
-  return commit('holoWorldEntry', entry)
-}
-
-function holoWorldEntryRead (hash) {
-  return JSON.parse(get(hash))
 }
