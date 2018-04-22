fabb2b578de41a24a1430b6d45b9a05cedab31fc
Drew McArthur <drewmcarthur1@gmail.com>
2018-04-22 19:59:55 -0400

    add comments and formatting to ui/*

--- ui/HoloWorld.js
+++ ui/HoloWorld.js
@@ -1,25 +1,55 @@
-function holoWorldEntryCreate (message, callback) {
+// create the ui-facing function that allows the creation of a holoText entry
+// create a new XML-formatted http request.  POST to the url with json data
+// entry, a timestamp, and a callback function.
+function holoWorldEntryCreate (entry, callback) {
+
+  // url to POST request to 
+  var url = '/fn/HoloWorld/holoWorldEntryCreate'
+
+  // create a new request, which will be a POST to the url above, with JSON data
   var xhr = new XMLHttpRequest()
-  var url = '/fn/readerWriter/holoTextWrite'
-  xhr.open('POST', url, true)
+  xhr.open('Post', url, true)
   xhr.setRequestHeader('Content-type', 'application/json')
+
+  // add the given callback to the request to be called when its state changes.
   xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      callback(xhr.responseText)
+    if (hr.readyState === 4 && xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText))
     }
   }
-  xhr.send(message)
+
+  // ensure that the Date.now object is initialized
+  // https://stackoverflow.com/a/221297/3972042
+  //if (!Date.now) {
+        //Date.now = function() { return new Date().getTime(); }
+  //}
+
+  // format the data and send it via the constructed request
+  var data = JSON.stringify({'content': entry, 'timestamp': Date.now()})
+  xhr.send(data)
 }
 
+// create the ui-facing function that allows reading a holoText entry
+// create a new XML-formatted http request.  POST to the url with the hash
+// and a callback function to be called on the retrieved entry
 function holoWorldEntryRead (hash, callback) {
+
+  // the url to post to
+  var url = '/fn/HoloWorld/holoWorldEntryRead'
+  
+  // create the new request object & configure it to POST json data to url 
   var xhr = new XMLHttpRequest()
-  var url = '/fn/readerWriter/holoTextRead'
   xhr.open('POST', url, true)
   xhr.setRequestHeader('Content-type', 'application/json')
+
+  // add a callback function to be called on the response 
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      callback(xhr.responseText)
+      callback(JSON.parse(xhr.responseText))
     }
   }
-  xhr.send(hash)
+
+  // format and send the hash through the request.
+  var data = JSON.stringify(hash)
+  xhr.send(data)
 }
